6ea2e4334db75d7c40dcfeb415e80eabf7020846
luoliufei <luoliufei@szca.com>
2018-05-15 17:35:00 +0800

    [BJCSZCA-1]

--- bcia-ca-szca-publicweb/src/main/java/org/bcia/javachain/ca/szca/publicweb/api/CallConfigData.java
+++ bcia-ca-szca-publicweb/src/main/java/org/bcia/javachain/ca/szca/publicweb/api/CallConfigData.java
@@ -1,21 +1,19 @@
 /*
- * **
- *  *
- *  * Copyright © 2018  深圳市电子商务安全证书管理有限公司(SZCA,深圳CA) 版权所有
- *  * Copyright © 2018  SZCA. All Rights Reserved.
- *  * <p>
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  * <p>
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  * <p>
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *  *
+ *
+ * Copyright © 2018  深圳市电子商务安全证书管理有限公司(SZCA,深圳CA) 版权所有
+ * Copyright © 2018  SZCA. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -45,11 +43,7 @@ import javax.persistence.Table;
 import org.apache.log4j.Logger;
 import org.hibernate.annotations.Type;
 
-/**
- * Representation of a CA instance.
- * 
- * @version $Id: CAData.java 22592 2016-01-18 12:09:44Z marko $
- */
+
 @Entity
 @Table(name = "api_call_config")
 public class CallConfigData implements Serializable {
