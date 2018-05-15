d5fa3fb428b5e6de0c8aa2668b8d186eef372881
luoliufei <luoliufei@szca.com>
2018-05-15 17:30:18 +0800

    [BJCSZCA-1]

---
+++ bcia-ca-szca-publicweb/src/main/java/org/bcia/javachain/ca/szca/publicweb/api/ApiAdminUserData.java
@@ -0,0 +1,77 @@
+
+/*
+ * **
+ *  *
+ *  * Copyright © 2018  深圳市电子商务安全证书管理有限公司(SZCA,深圳CA) 版权所有
+ *  * Copyright © 2018  SZCA. All Rights Reserved.
+ *  * <p>
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  * <p>
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  * <p>
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *
+ *
+ */
+
+package org.bcia.javachain.ca.szca.publicweb.api;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.log4j.Logger;
+import org.hibernate.annotations.Type;
+
+ 
+/**
+ * @author Jacky Luo
+ *
+ */
+@Entity
+@Table(name = "api_user_config")
+public class ApiAdminUserData implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(ApiAdminUserData.class);
+ 
+
+	private String username;
+	private String password;
+
+	
+	 
+	@Id
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	 
+}
