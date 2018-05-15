69a7efa2480ce10d607891f91e73b6dee3d9346f
luoliufei <luoliufei@szca.com>
2018-05-15 18:48:02 +0800

    [BJCSZCA-1]

--- bcia-ca-szca-publicweb/src/main/java/org/bcia/javachain/ca/szca/publicweb/api/CallConfigData.java
+++ bcia-ca-szca-publicweb/src/main/java/org/bcia/javachain/ca/szca/publicweb/api/CallConfigData.java
@@ -17,18 +17,6 @@
  *
  */
 
-/*************************************************************************
- *                                                                       *
- *  CESeCore: CE Security Core                                           *
- *                                                                       *
- *  This software is free software; you can redistribute it and/or       *
- *  modify it under the terms of the GNU Lesser General Public           *
- *  License as published by the Free Software Foundation; either         *
- *  version 2.1 of the License, or any later version.                    *
- *                                                                       *
- *  See terms of license at gnu.org.                                     *
- *                                                                       *
- *************************************************************************/
 package org.bcia.javachain.ca.szca.publicweb.api;
 
 import java.io.Serializable;
